b3e9bb755533854c84ea85b1399e9ec0b2f82a76
soniapetrini <soniajpetrini@gmail.com>
2025-11-10 09:07:19 +0100

    plots in pdf

--- code/main_results.R
+++ code/main_results.R
@@ -89,7 +89,7 @@ lapply(outcomes, function(Outcome) {
     plot_perc(res)
     
     # Save
-    ggsave(paste0("plots/",ds,"_",Outcome,"_",fun_pred,"_terciles.tiff"),
+    ggsave(paste0("plots/",ds,"_",Outcome,"_",fun_pred,"_terciles.pdf"),
            width = 8, height =7, dpi=300)
     
   })
